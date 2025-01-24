c9dacd75470983a1d5c75e1688e0aafbce10a35d
chin wei keat <THX4IQU@users.noreply.huggingface.co>
2025-01-24 18:17:39 +0000

    Update app.py

--- app.py
+++ app.py
@@ -293,8 +293,8 @@ with gr.Blocks(delete_cache=(600, 600)) as demo:
             generate_btn = gr.Button("Generate")
             
             with gr.Accordion(label="GLB Extraction Settings", open=False):
-                mesh_simplify = gr.Slider(0.9, 0.98, label="Simplify", value=0.95, step=0.01)
-                texture_size = gr.Slider(512, 2048, label="Texture Size", value=1024, step=512)
+                mesh_simplify = gr.Slider(0.5, 0.98, label="Simplify", value=0.95, step=0.01)
+                texture_size = gr.Slider(512, 4096, label="Texture Size", value=1024, step=512)
             
             with gr.Row():
                 extract_glb_btn = gr.Button("Extract GLB", interactive=False)
